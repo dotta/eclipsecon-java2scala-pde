98c5743f389377260918bd777a17b855ab02b4bb
Mirco Dotta <mirco.dotta@typesafe.com>
2013-10-28 09:34:56 +0100

    base
    Running it will use the Java SampleHandler

---
+++ src/org/scalaide/eclipsecon/javaplugin/handlers/SampleHandler2.scala
@@ -0,0 +1,29 @@
+//package org.scalaide.eclipsecon.javaplugin.handlers
+//
+//import org.eclipse.core.commands.AbstractHandler;
+//import org.eclipse.core.commands.ExecutionEvent;
+//import org.eclipse.core.commands.ExecutionException;
+//import org.eclipse.ui.IWorkbenchWindow;
+//import org.eclipse.ui.handlers.HandlerUtil;
+//import org.eclipse.jface.dialogs.MessageDialog;
+//
+///**
+// * Our sample handler extends AbstractHandler, an IHandler base class.
+// * @see org.eclipse.core.commands.IHandler
+// * @see org.eclipse.core.commands.AbstractHandler
+// */
+//class SampleHandler2 extends AbstractHandler {
+//
+//	/**
+//	 * the command has been executed, so extract extract the needed information
+//	 * from the application context.
+//	 */
+//	override def execute(event: ExecutionEvent): Object = {
+//		val window = HandlerUtil.getActiveWorkbenchWindowChecked(event)
+//		MessageDialog.openInformation(
+//				window.getShell(),
+//				"Scala plugin",
+//				"Hello, Eclipse world")
+//		null
+//	}
+//}
